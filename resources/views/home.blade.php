a2f6e0a0efaeba1a1a0acc7e1417fc7553db7c0b
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-06-29 17:37:27 +0200

    bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,5 +2,5 @@
 
 
 @section('content')
-home
+Comics
 @endsection
---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+    <div class="collapse navbar-collapse " id="navbarNav">
+      <ul class="w-100 navbar-nav d-flex justify-content-center">
+
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('home')}}">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="{{route('comics.index')}}">Comics</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
